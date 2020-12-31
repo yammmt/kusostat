9e529337a62ded02c3d2d306bbf0006ebaf6d1f5
yammmt <cu.cs.y20@gmail.com>
2020-12-31 22:00:43 +0900

    Show attribute name instead of foreign key (number) itself
    This commit lets us to see readable name instead of internal primary key
    (id/number). For example, "color: 1" in the previous commit is changed
    to "color: brown".
    
    This commit drops support of original `poo` table in actual database.
    It's because the users don't have to take care of original id/name
    combination used in database.

--- src/db/poo.rs
+++ src/db/poo.rs
@@ -2,15 +2,15 @@ use chrono::{NaiveDateTime, NaiveTime};
 use diesel::{self, prelude::*};
 use serde::Serialize;
 
-use crate::schema::poo;
 use crate::schema::poo::dsl::poo as all_poos;
+use crate::schema::*;
 
 #[derive(Clone, Debug, Eq, PartialEq, Serialize, Queryable)]
 pub struct Poo {
     id: i32,
-    pub form: i32,
-    pub color: i32,
-    pub bleeding: i32,
+    pub form: String,
+    pub color: String,
+    pub bleeding: String,
     pub required_time: NaiveTime,
     pub published_at: NaiveDateTime,
 }
@@ -19,7 +19,18 @@ impl Poo {
     pub fn all(conn: &PgConnection) -> Vec<Poo> {
         all_poos
             .order(poo::published_at.desc())
-            .load::<Poo>(conn)
+            .inner_join(poo_form::table)
+            .inner_join(poo_color::table)
+            .inner_join(poo_bleeding::table)
+            .select((
+                poo::id,
+                poo_form::name,
+                poo_color::name,
+                poo_bleeding::name,
+                poo::required_time,
+                poo::published_at,
+            ))
+            .load(conn)
             .expect("Failed to read data from DB")
     }
 }
